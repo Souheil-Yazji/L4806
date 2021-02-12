3cd84ffe21b61e99cca497db7d616cd249acd620
Souheil-Yazji <35379392+Souheil-Yazji@users.noreply.github.com>
2021-02-12 10:49:00 -0500

    Updated MVC test class

--- src/test/java/SYSoft/AddressBook/TestingWebApplicationTest.java
+++ src/test/java/SYSoft/AddressBook/TestingWebApplicationTest.java
@@ -10,6 +10,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,6 +24,7 @@ public class TestingWebApplicationTest {
     private MockMvc mockMvc;
 
     private AddressBook book;
+    private BuddyInfo buddy = new BuddyInfo("Test", "testAddr", "1");
 
     @Test
     public void postAddressBookTest() throws Exception {
@@ -31,19 +35,24 @@ public class TestingWebApplicationTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+
+        this.mockMvc.perform( MockMvcRequestBuilders
+                .post("/addressbook/buddy?bookId=1")
+                .content(asJsonString(buddy))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("test")));
+
+        this.mockMvc.perform( MockMvcRequestBuilders
+                .delete("/addressbook/buddy?bookId=1&buddyId=2")
+                .content(asJsonString(buddy))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(not(containsString("test"))));
     }
 
-//    @Test
-//    public void getAddressBookTest() throws Exception {
-//        this.mockMvc.perform( MockMvcRequestBuilders
-//                .get("/addressbook")
-//                .content(asJsonString(book))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .param("id")
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
-//    }
 
     public static String asJsonString(final Object obj) {
         try {
